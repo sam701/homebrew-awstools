fc3fc5221238305b86d8ee5506cb964b48d94249
Christian Pelster <cpelster@thoughtworks.com>
2016-09-21 23:07:51 +0200

    initial commit

---
+++ awstools.rb
@@ -0,0 +1,21 @@
+class Awstools < Formula
+  desc "blabla"
+  homepage "homepage"
+  url "https://github.com/sam701/awstools/releases/download/0.9.3/awstools_darwin_amd64"
+  version "0.9.3"
+  sha256 "b61b8aaff1853cfc68b8b7d1949278089d076fa50ffde0784cc633b68fdcb993"
+
+  bottle :unneeded
+
+  def install
+    mv "awstools_darwin_amd64", "awstools"
+    bin.install "awstools"
+  end
+
+  test do
+    actual = pipe_output("#{bin}/awstools --version")
+    expected = "awstools version 0.9.3\n"
+    assert_equal expected, actual
+  end
+  
+end
