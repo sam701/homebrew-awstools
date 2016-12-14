74748b338b8904dc5325300fa6273eb6ca5ef653
Alexei Samokvalov <a.sam701@gmail.com>
2016-12-14 13:43:04 +0100

    Version 0.10.0

--- awstools.rb
+++ awstools.rb
@@ -1,9 +1,9 @@
 class Awstools < Formula
   desc "A few helpful AWS tools"
   homepage "https://github.com/sam701/awstools"
-  url "https://github.com/sam701/awstools/releases/download/0.9.3/awstools_darwin_amd64"
-  version "0.9.3"
-  sha256 "b61b8aaff1853cfc68b8b7d1949278089d076fa50ffde0784cc633b68fdcb993"
+  url "https://github.com/sam701/awstools/releases/download/0.10.0/awstools_darwin_amd64"
+  version "0.10.0"
+  sha256 "bb677c4675f2d7f57fba145362b68598e935533bb532a7027238a17676212e45"
 
   bottle :unneeded
 
@@ -14,7 +14,7 @@ class Awstools < Formula
 
   test do
     actual = pipe_output("#{bin}/awstools --version")
-    expected = "awstools version 0.9.3\n"
+    expected = "awstools version 0.10.0\n"
     assert_equal expected, actual
   end
   
